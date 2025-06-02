0ccaa3178612950f4f2a8e806d35b3fbaf29c8df
mlkeen <mlstockton@gmail.com>
2025-06-01 23:44:38 -0400

    Updated time functions

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,9 +1,12 @@
 from app.models import GameState
-from datetime import datetime
+from datetime import datetime, timezone
 
 def base_context():
     game_state = GameState.query.first()
+    now = datetime.now(timezone.utc)
+    fictional_time = now.replace(year=now.year + 250)
     return {
         "game_state": game_state,
-        "now": datetime.utcnow
+        "now": now,
+        "fictional_time": fictional_time
     }
